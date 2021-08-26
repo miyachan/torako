5000b9a2bd110e834a541fb04b44a4c2d6afe740
miyachan <wakarimasenco@gmail.com>
2021-08-26 16:50:17 -0700

    lnx: Fix document format

--- src/storage/search_lnx/post.rs
+++ src/storage/search_lnx/post.rs
@@ -1,41 +1,62 @@
-use serde::Serialize;
+use serde::{Serialize, Serializer};
+
+fn field_ser<T: Serialize, S: Serializer>(x: T, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    [&x].serialize(s)
+}
 
 #[derive(Serialize, Clone, Debug)]
 pub struct Post<'a> {
+    #[serde(serialize_with = "field_ser")]
     board: &'static str,
+    #[serde(serialize_with = "field_ser")]
     thread_no: u64,
+    #[serde(serialize_with = "field_ser")]
     post_no: u64,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     subject: Option<&'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     username: Option<&'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     tripcode: Option<&'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     email: Option<&'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     unique_id: Option<&'a str>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     since4_pass: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     country: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     filename: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     image_hash: Option<&'a str>,
+    #[serde(serialize_with = "field_ser")]
     image_width: u64,
+    #[serde(serialize_with = "field_ser")]
     image_height: u64,
+    #[serde(serialize_with = "field_ser")]
     ts: u64,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     comment: Option<String>,
+    #[serde(serialize_with = "field_ser")]
     deleted: u64,
+    #[serde(serialize_with = "field_ser")]
     ghost: u64,
+    #[serde(serialize_with = "field_ser")]
     sticky: u64,
+    #[serde(serialize_with = "field_ser")]
     spoiler: u64,
+    #[serde(serialize_with = "field_ser")]
     op: u64,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none", serialize_with = "field_ser")]
     capcode: Option<u64>,
 }
+
+
+
 
 impl<'a> From<&'a crate::imageboard::Post> for Post<'a> {
     fn from(post: &'a crate::imageboard::Post) -> Self {
